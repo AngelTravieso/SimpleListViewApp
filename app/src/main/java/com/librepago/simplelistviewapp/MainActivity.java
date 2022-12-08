16102ca5657be52bbd7ae110b6a0594b92a0f872
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-08 18:02:54 -0400

    array adapter
    simple list view

---
+++ app/src/main/java/com/librepago/simplelistviewapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.librepago.simplelistviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 1. AdapterView
+    ListView listView;
+
+    // 2. Origen de datos (data source)
+    String[] worldCup22 = {
+            "Germany",
+            "Spain",
+            "Brazil",
+            "England",
+            "USA",
+            "Saudi Arabia",
+            "Qatar",
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R .layout.activity_main);
+
+        // inicializar widget
+        listView = findViewById(R.id.listView);
+
+        // 3. Adapter
+        ArrayAdapter adapter = new ArrayAdapter(
+
+        );
+
+
+
+    }
+}
